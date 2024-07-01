68ffa0ac7d7e05b3f1638f784fce123b5ea8e684
swarat <swaratvaghela@gmail.com>
2024-07-01 11:07:26 +0530

    LEC 1

---
+++ GOFOOD/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from './screens/Home'
+import {BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ GOFOOD/src/screens/Home.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Home = ()=>{
+  return(<>
+
+              <Navbar></Navbar>
+              <div>Body</div>
+              <div> <Footer/></div>
+  </>);
+}
+export default Home;
